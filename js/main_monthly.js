27be8b77cbd98b07cb1c71c3807fd3f7beb7b7fc
Jessica Steslow <124192196+steslowj@users.noreply.github.com>
2023-07-01 13:05:20 -0700

    Lab 1 Draft
    Converted the activity exercises into a webpage for displaying Air Quality Index data, as a draft for Lab 1.

--- js/main_monthly.js
+++ js/main_monthly.js
@@ -1,51 +1,49 @@
-/*Author: Jessica Steslow, 6-19-2023*/
+/*Author: Jessica Steslow, 6-27-2023*/
 
 //declare map variable globally so all functions have access
-var map;
+var map_mon;
 var minValue;
+var calendar = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"]
 
 //function to instantiate the Leaflet map
-function createMap(){
+function createMapMon(){
     
     //create the map, centered apprx. on the center of my US city data
-    map = L.map('map', {
+    map_mon = L.map('mapcard-monthly', {
         center: [38, -97],
         zoom: 4
     });
-
 
     L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
         subdomains: 'abcd',
         maxZoom: 20
-    }).addTo(map);
+    }).addTo(map_mon);
     
     //call getData function
-    getData(map);
+    getDataMon(map_mon);
 };
 
-
-function calcMinValue(data){
+function calcMinValueMon(data){
     //create empty array to store all data values
     var allValues = [];
     //loop through each city
     for(var city of data.features){
-        //loop through each year
-        for(var year = 2016; year <= 2022; year+=1){
-              //get population for current year
-              var value = city.properties["medAQI-"+ String(year)];
-              //add value to array
-              allValues.push(value);
+        //loop through each 12 month cycle
+        for(var i = 0; i <=11; i+=1){
+            //get air quality for current month
+            var value = city.properties[calendar[i]+String("_2021")];
+            //add value to array
+            allValues.push(value);
         }
     }
     //get minimum value of our array
     var minValue = Math.min(...allValues)
-
     return minValue;
 }
 
 //calculate the radius of each proportional symbol
-function calcPropRadius(attValue) {
+function calcPropRadiusMon(attValue) {
     //constant factor adjusts symbol sizes evenly
     var minRadius = 6;
     //Flannery Apperance Compensation formula
@@ -54,7 +52,7 @@ function calcPropRadius(attValue) {
 };
 
 //function to categorize proportional symbol color by AQI value
-function groupPropColor(attValue) {
+function groupPropColorMon(attValue) {
     var aqiColor = "";
     if ( attValue <= 50) {           //Green, Good
         aqiColor = "#00e400";
@@ -89,10 +87,10 @@ function pointToLayer(feature, latlng, attributes){
     var attValue = Number(feature.properties[attribute]);
 
     //Give each feature's circle marker a radius based on its attribute value
-    options.radius = calcPropRadius(attValue);
+    options.radius = calcPropRadiusMon(attValue);
 
     //Give each featur's circle marker a color based on its attribute value
-    options.fillColor = groupPropColor(attValue);
+    options.fillColor = groupPropColorMon(attValue);
 
     //create circle marker layer
     var layer = L.circleMarker(latlng, options);
@@ -101,8 +99,8 @@ function pointToLayer(feature, latlng, attributes){
     var popupContent = "<p><b>Metro:</b> " + feature.properties.CBSA + "</p>";
 
     //add formatted attribute to popup content string
-    var year = attribute.split("-")[1];
-    popupContent += "<p><b>Median AQI in " + year + ":</b> " + feature.properties[attribute] + "</p>";
+    var month = attribute.split("_")[0];
+    popupContent += "<p><b>Median AQI in " + month + " 2021:</b> " + feature.properties[attribute] + "</p>";
 
     //bind the popup to the circle marker
     layer.bindPopup(popupContent, {
@@ -113,37 +111,37 @@ function pointToLayer(feature, latlng, attributes){
     return layer;
 };
 
-function createPropSymbols(data, attributes){
+function createPropSymbolsMon(data, attributes){
     //create a Leaflet GeoJSON layer and add it to the map
     L.geoJson(data, {
         pointToLayer: function(feature, latlng){
             return pointToLayer(feature, latlng, attributes);
         }
-    }).addTo(map);
+    }).addTo(map_mon);
 };
 
 //Step 10: Resize proportional symbols according to new attribute values
-function updatePropSymbols(attribute){
-    map.eachLayer(function(layer){
+function updatePropSymbolsMon(attribute){
+    map_mon.eachLayer(function(layer){
 
         if (layer.feature && layer.feature.properties[attribute]){
           //access feature properties
            var props = layer.feature.properties;
 
            //update each feature's radius based on new attribute values
-           var radius = calcPropRadius(props[attribute]);
+           var radius = calcPropRadiusMon(props[attribute]);
            layer.setRadius(radius);
 
            //update each feature's color based on attribute values
-           var newColor = groupPropColor(props[attribute]);
+           var newColor = groupPropColorMon(props[attribute]);
            layer.setStyle({fillColor: newColor});
 
            //add city to popup content string
            var popupContent = "<p><b>Metro:</b> " + props.CBSA + "</p>";
 
            //add formatted attribute to panel content string
-           var year = attribute.split("-")[1];
-           popupContent += "<p><b>Median AQI in " + year + ":</b> " + props[attribute] + "</p>";
+           var month = attribute.split("_")[0];
+           popupContent += "<p><b>Median AQI in " + month + " 2021:</b> " + props[attribute] + "</p>";
 
            //update popup with new content
            popup = layer.getPopup();
@@ -153,7 +151,7 @@ function updatePropSymbols(attribute){
     });
 };
 
-function processData(data){
+function processDataMon(data){
     //empty array to hold attributes
     var attributes = [];
 
@@ -163,7 +161,7 @@ function processData(data){
     //push each attribute name into attributes array
     for (var attribute in properties){
         //only take attributes with Air Quality Index values
-        if (attribute.indexOf("AQI") > -1){
+        if (attribute.indexOf("_2021") > -1){
             attributes.push(attribute);
         };
     };
@@ -173,9 +171,8 @@ function processData(data){
 
 
 //Create new sequence controls
-function createSequenceControls(attributes){   
+function createSequenceControlsMon(attributes){   
     
-    //This block is specific to 'Chapter 6 solutions', not in Lab 1 Mod 2, but seems to be necessary for the slider
     var SequenceControl = L.Control.extend({
         options: {
             position: 'bottomleft'
@@ -183,104 +180,79 @@ function createSequenceControls(attributes){
 
         onAdd: function () {
             // create the control container div with a particular class name
-            var container = L.DomUtil.create('div', 'sequence-control-container');
+            var container = L.DomUtil.create('div', 'sequence-control-container-mon');
 
             //create range input element (slider)
-            container.insertAdjacentHTML('beforeend', '<input class="range-slider" type="range">')
+            container.insertAdjacentHTML('beforeend', '<input class="range-slider-mon" type="range">')
 
             //add skip buttons
-            container.insertAdjacentHTML('beforeend', '<button class="step" id="reverse" title="Reverse"><img src="img/arrow_left.png"></button>'); 
-            container.insertAdjacentHTML('beforeend', '<button class="step" id="forward" title="Forward"><img src="img/arrow_right.png"></button>'); 
+            container.insertAdjacentHTML('beforeend', '<button class="step-mon" id="reverse" title="Reverse"><img src="img/arrow_left.png"></button>'); 
+            container.insertAdjacentHTML('beforeend', '<button class="step-mon" id="forward" title="Forward"><img src="img/arrow_right.png"></button>'); 
 
             //disable any mouse event listeners for the container
             L.DomEvent.disableClickPropagation(container);
 
-
             return container;
-
         }
     });
     
 
-    /*
-        //Code from Lab 1 Module 2 that doesn't seem to work with the rest of the code?
-
-        //create range input element (slider)
-        var slider = "<input class='range-slider' type='range'></input>";
-        document.querySelector("#panel").insertAdjacentHTML('beforeend',slider);
-
-        //set slider attributes
-        document.querySelector(".range-slider").max = 6;
-        document.querySelector(".range-slider").min = 0;
-        document.querySelector(".range-slider").value = 0;
-        document.querySelector(".range-slider").step = 1;
-
-        //add step buttons
-        document.querySelector('#panel').insertAdjacentHTML('beforeend','<button class="step" id="reverse">Reverse</button>');
-        document.querySelector('#panel').insertAdjacentHTML('beforeend','<button class="step" id="forward">Forward</button>');
-
-        //replace button content with images
-        document.querySelector('#reverse').insertAdjacentHTML('afterbegin',"<img src='img/arrow_left.png'>")
-        document.querySelector('#forward').insertAdjacentHTML('afterbegin',"<img src='img/arrow_right.png'>")
-    */
-
-    map.addControl(new SequenceControl());
+    map_mon.addControl(new SequenceControl());
 
     ///////add listeners after adding the control!///////
     //set slider attributes
-    document.querySelector(".range-slider").max = 6;
-    document.querySelector(".range-slider").min = 0;
-    document.querySelector(".range-slider").value = 0;
-    document.querySelector(".range-slider").step = 1;
+    document.querySelector(".range-slider-mon").max = 11;
+    document.querySelector(".range-slider-mon").min = 0;
+    document.querySelector(".range-slider-mon").value = 0;
+    document.querySelector(".range-slider-mon").step = 1;
 
-    var steps = document.querySelectorAll('.step');
+    var steps = document.querySelectorAll('.step-mon');
 
     steps.forEach(function(step){
         step.addEventListener("click", function(){
-            var index = document.querySelector('.range-slider').value;
+            var index = document.querySelector('.range-slider-mon').value;
             //Step 6: increment or decrement depending on button clicked
             if (step.id == 'forward'){
                 index++;
                 //Step 7: if past the last attribute, wrap around to first attribute
-                index = index > 6 ? 0 : index;
+                index = index > 11 ? 0 : index;
             } else if (step.id == 'reverse'){
                 index--;
                 //Step 7: if past the first attribute, wrap around to last attribute
-                index = index < 0 ? 6 : index;
+                index = index < 0 ? 11 : index;
             };
 
             //Step 8: update slider
-            document.querySelector('.range-slider').value = index;
+            document.querySelector('.range-slider-mon').value = index;
 
             //Step 9: pass new attribute to update symbols
-            updatePropSymbols(attributes[index]);
+            updatePropSymbolsMon(attributes[index]);
         })
     })
 
     //Step 5: input listener for slider
-    document.querySelector('.range-slider').addEventListener('input', function(){
+    document.querySelector('.range-slider-mon').addEventListener('input', function(){
         //Step 6: get the new index value
         var index = this.value;
 
         //Step 9: pass new attribute to update symbols
-        updatePropSymbols(attributes[index]);
+        updatePropSymbolsMon(attributes[index]);
     });
-
 };
 
-function getData(map){
+function getDataMon(map_mon){
     //load the data
-    fetch("data/AQI_annual.geojson")
+    fetch("data/AQI_2021monthly.geojson")
         .then(function(response){
             return response.json();
         })
         .then(function(json){
-            var attributes = processData(json);
-            minValue = calcMinValue(json);
+            var attributes = processDataMon(json);
+            minValue = calcMinValueMon(json);
             //call function to create proportional symbols
-            createPropSymbols(json, attributes);
-            createSequenceControls(attributes);
+            createPropSymbolsMon(json, attributes);
+            createSequenceControlsMon(attributes);
         })
 };
 
-document.addEventListener('DOMContentLoaded',createMap);
+document.addEventListener('DOMContentLoaded',createMapMon);
